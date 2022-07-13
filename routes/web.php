a393e2bafc3e2698f9049a4e7f97ae6cb87840eb
Rifka Yuliana <rifka.yuliana.putri-2020@vokasi.unair.ac.id>
2022-07-13 12:16:18 +0700

    uptemplate2

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
 });
 
