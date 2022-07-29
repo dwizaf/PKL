da3db55d915b3c65f37462c9ef25f232acf4c685
Sabrinafitrip <sabrina.fitri.puspitasari-2020@vokasi.unair.ac.id>
2022-07-29 11:26:26 +0700

    dashboard1

--- storage/framework/views/dfb26b04933f75e453bb10b027b19b27e17ca3f5.php
+++ storage/framework/views/dfb26b04933f75e453bb10b027b19b27e17ca3f5.php
@@ -8,6 +8,7 @@
 <body>
 
     <div class="container-fluid mt-3">
+        
         <div class="row">
             <div class="col-lg-3 col-sm-6">
                 <div class="card gradient-1">
@@ -58,6 +59,48 @@
                 </div>
             </div>
         </div>
+        
+        
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="row">
+                    <div class="col-12">
+
+                        <div class="card">
+                            <div class="card-body pb-0 d-flex justify-content-between">
+                                <div>
+                                    <h3 class="mb-1">Tentang Website Ini</h3><br>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elitconsectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud Exercitation ullamco laboris
+                                        nisi ut aliquip ex ea commodo consequat.</p>
+                                </div>
+                            </div>
+                        </div>
+    
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="inline-editor">
+                                    <h3>Kedudukan dan Alamat</h3><br>
+                                    <h5>Kedudukan</h5>
+                                    <p>Dinas Komunikasi dan Inforrnatika merupakan unsur pelaksana otonomi daerah, 
+                                        dipimpin oleh seorang kepala dinas, yang berada di bawah dan bertanggung jawab kepada 
+                                        Gubernur melalui Sekretaris Daerah.</p>
+                                    <h5>Alamat</h5>
+                                    <p>Dinas Komunikasi dan Informatika Pemerintah Provinsi Jawa Timur</p>
+                                    <p>Jl. A Yani 242-244 Surabaya</p>
+                                    <ul class="list-icons">
+                                        <li><i class="fa fa-check text-success"></i> Email : kominfo@jatimprov.go.id</li>
+                                        <li><i class="fa fa-check text-success"></i> Website : https://kominfo.jatimprov.go.id</li>
+                                        <li><i class="fa fa-check text-success"></i> Telp. (031) 8294608, Fax (031) 8294517</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 
 <?php $__env->stopSection(); ?>
